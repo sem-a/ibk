96c7b5be1df2fb245156c8ae71b9e7dfb68b5659
Алексей С. <76011034+sem-a@users.noreply.github.com>
2025-06-20 17:37:10 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -121,21 +121,3 @@ class Slider {
     }
   }
 }
-
-document.addEventListener("DOMContentLoaded", () => {
-  const body = document.querySelector("body");
-  const burgerButton = document.querySelector(".nav__burger");
-  const navList = document.querySelector("header");
-
-  burgerButton.addEventListener("click", () => {
-    if (navList.classList.contains("nav-active")) {
-      body.classList.remove("modal-open");
-      navList.classList.remove("nav-active");
-      burgerButton.children[0].classList.remove("nav-active");
-    } else {
-      body.classList.add("modal-open");
-      navList.classList.add("nav-active");
-      burgerButton.children[0].classList.add("nav-active");
-    }
-  });
-});
